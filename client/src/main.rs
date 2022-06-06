6974bdae8c7ec6443a0132c24ad8be3283ca88db
Swann HERRERA <swann@devloup.dev>
2022-06-06 10:53:51 +0200

    send length before message and parse it

--- client/src/main.rs
+++ client/src/main.rs
@@ -9,7 +9,7 @@ fn main() {
     let address = format!("{}:{}", IP, PORT);
     match TcpStream::connect(address) {
         Ok(stream) => {
-            let message = Message::Subscribe { name: "test".to_string() };
+            let message = Message::Subscribe { name: "tesa;ihilah;iuvbasd;iuv'ouwdnwbdvkjbwdjkvbijwdkhbvi;bwDKVBJKWDBVIHWIOUFHIWHBFIHWFOHIWUHFOHWFOUJWEOFHIOWEJFOIJwbdvkjbwdjkvbijwdkhbvi;bwDKVBJKWDBVIHWIOUFHIWHBFIHWFOHIWUHFOHWFOUJWEOFHIOWEJFOIJwbdvkjbwdjkvbijwdkhbvi;bwDKVBJKWDBVIHWIOUFHIWHBFIHWFOHIWUHFOHWFOUJWEOFHIOWEJFOIJwbdvkjbwdjkvbijwdkhbvi;bwDKVBJKWDBVIHWIOUFHIWHBFIHWFOHIWUHFOHWFOUJWEOFHIOWEJFOIJwbdvkjbwdjkvbijwdkhbvi;bwDKVBJKWDBVIHWIOUFHIWHBFIHWFOHIWUHFOHWFOUJWEOFHIOWEJFOIJwbdvkjbwdjkvbijwdkhbvi;bwDKVBJKWDBVIHWIOUFHIWHBFIHWFOHIWUHFOHWFOUJWEOFHIOWEJFOIJwbdvkjbwdjkvbijwdkhbvi;bwDKVBJKWDBVIHWIOUFHIWHBFIHWFOHIWUHFOHWFOUJWEOFHIOWEJFOIJwbdvkjbwdjkvbijwdkhbvi;bwDKVBJKWDBVIHWIOUFHIWHBFIHWFOHIWUHFOHWFOUJWEOFHIOWEJFOIJvk;wbdvkjbwdjkvbijwdkhbvi;bwDKVBJKWDBVIHWIOUFHIWHBFIHWFOHIWUHFOHWFOUJWEOFHIOWEJFOIJt".to_string() };
             send_message(stream, message);
         },
         Err(_) => panic!("Could not connect to server {} on port {}", IP, PORT),
@@ -18,7 +18,11 @@ fn main() {
 
 fn send_message(mut stream: TcpStream, message: Message) {
     if let Ok(message) = serde_json::to_string(&message) {
-        let result = stream.write(message.as_bytes()).unwrap();
+        let bytes_message = message.as_bytes();
+        let message_size = bytes_message.len() as u32;
+        let message_length_as_bytes = message_size.to_be_bytes();
+        stream.write(&message_length_as_bytes).unwrap();
+        let result = stream.write(bytes_message).unwrap();
         println!("result : {}, message: {}", result, message);
     } 
 }
