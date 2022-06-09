a94cea88a83852df21846bbfb6fef7093f1c77e1
pooetitu <pooetitu@gmail.com>
2022-06-09 20:25:18 +0200

    implemented connection to server and accepted user connection

--- client/src/main.rs
+++ client/src/main.rs
@@ -9,24 +9,34 @@ fn main() {
     let address = format!("{}:{}", IP, PORT);
     match TcpStream::connect(address) {
         Ok(stream) => {
+            stream.set_nonblocking(true);
+            let message = Message::Hello;
+            send_message(&stream, message);
             let message = Message::Subscribe { name: "test".to_string() };
-            send_message(stream, message);
+            send_message(&stream, message);
+            receive_messages(&stream);
         },
         Err(_) => panic!("Could not connect to server {} on port {}", IP, PORT),
     }
 }
 
-fn send_message(mut stream: TcpStream, message: Message) {
+fn send_message(mut stream: &TcpStream, message: Message) {
     if let Ok(message) = serde_json::to_string(&message) {
         let bytes_message = message.as_bytes();
         let message_size = bytes_message.len() as u32;
         let message_length_as_bytes = message_size.to_be_bytes();
-        let result =  stream.write(&[&message_length_as_bytes, bytes_message].concat());
+        let result = stream.write(&[&message_length_as_bytes, bytes_message].concat());
         println!("result : {:?}, message: {}", result, message);
-        loop  {
-            let mut buf = [0; 1024];
-            let message = stream.read(&mut buf);
-            println!("{:?}", message);
+    }
+}
+
+fn receive_messages(mut stream: &TcpStream){
+    loop {
+        let mut v = Vec::<u8>::new();
+        stream.read_to_end(&mut v);
+        let str = String::from_utf8_lossy(&v);
+        if str != "" {
+            println!("{str:?}");
         }
-    } 
+    }
 }
