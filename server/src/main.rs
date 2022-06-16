252b7bf9a9fe841a950ae0c6b3e7051ba88a7893
Swann HERRERA <swann@devloup.dev>
2022-06-16 23:53:37 +0200

    handle message

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,61 +1,12 @@
-use std::net::{SocketAddr, TcpListener, TcpStream};
-use std::io::Read;
-use log::{info, debug};
 use message_handler::MessageHandler;
-use shared::config::{PORT, IP};
-use shared::message::Message;
+use server::{Server, create_listener};
+use shared::config::{self};
 
 mod message_handler;
-
-
-struct Server {
-    listener: TcpListener,
-    message_handler: MessageHandler,
-}
-
-impl Server {
-  fn new(listener: TcpListener, message_handler: MessageHandler) -> Server {
-    Server { listener, message_handler }
-  }
-
-  fn listen(&mut self) {
-    for message in self.listener.incoming() {
-      debug!("message={message:?}");
-      let message = self.parse_message_from_tcp_stream(message.unwrap());
-      let _response = self.message_handler.handle_message(message);
-      // TODO response
-    }
-  }
-
-  fn parse_message_from_tcp_stream(&self, mut message: TcpStream) -> Message {
-    let mut message_size = [0; 4];
-    let _size_error = message.read(&mut message_size);
-    let decimal_size = u32::from_be_bytes(message_size);
-
-    let mut bytes_of_message = vec![0; decimal_size as usize];
-    let _size_read = message.read_exact(&mut bytes_of_message);
-    let message = String::from_utf8_lossy(&bytes_of_message);
-    let message = serde_json::from_str(&message);
-    match message {
-      Ok(m) => m,
-      Err(err) => panic!("Cannot parse message : {err:?}")
-    }
-  }
-}
-
-
-fn create_listener() -> TcpListener {
-  let addr = SocketAddr::from((IP, PORT));
-  let listener = TcpListener::bind(addr);
-  info!("Start Listening on : {}", addr);
-  match listener {
-    Ok(l) => l,
-    Err(err) => panic!("Cannot listen on port : {err:?}")
-  }
-}
+mod server;
 
 fn main() {
-  std::env::set_var("RUST_LOG", "debug");
+  std::env::set_var("RUST_LOG", config::LOG_LEVEL);
   pretty_env_logger::init();
   let listener = create_listener();
   let message_handler = MessageHandler::new(vec![]);
