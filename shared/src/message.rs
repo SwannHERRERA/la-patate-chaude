d3cf23b48710cf15caec695d1d5aa9b1e0f47a5f
Swann HERRERA <swann@devloup.dev>
2022-06-28 10:38:26 +0200

    creation d'un workspace md5

--- shared/src/message.rs
+++ shared/src/message.rs
@@ -2,68 +2,12 @@ use serde::{Deserialize, Serialize};
 
 use crate::{
     public_player::PublicPlayer,
-    challenge::{MD5HashCash, ChallengeAnswer, ReportedChallengeResult},
-    subscribe::SubscribeResult
+    challenge::{ChallengeAnswer, ReportedChallengeResult, ChallengeType}, subscribe::SubscribeResult
 };
-
-#[derive(Serialize, Deserialize, Debug)]
-pub struct PublicPlayer {
-    pub name: String,
-    pub stream_id: String,
-    pub score: i32,
-    pub steps: u32,
-    pub is_active: bool,
-    pub total_used_time: f64,
-}
-
-#[derive(Serialize, Deserialize, Debug)]
-pub enum ChallengeAnswer {
-    MD5HashCash(MD5HashCashOutput),
-}
-
-#[derive(Serialize, Deserialize, Debug)]
-pub enum ChallengeValue {
-    Unreachable,
-    Timeout,
-    BadResult { used_time: f64, next_target: String },
-    Ok { used_time: f64, next_target: String },
-}
-
-#[derive(Serialize, Deserialize, Debug)]
-pub struct ReportedChallengeResult {
-    pub name: String,
-    pub value: ChallengeValue,
-}
 
 pub type PublicLeaderBoard = Vec<PublicPlayer>;
 
-#[derive(Serialize, Deserialize, Debug)]
-pub enum SubscribeResult {
-    Ok,
-    Err(SubscribeError),
-}
-
-#[derive(Serialize, Deserialize, Debug)]
-pub struct MD5HashCashOutput {
-    pub seed: u64,
-    pub hashcode: String,
-}
-
-#[derive(Serialize, Deserialize, Debug)]
-pub struct MD5HashCashInput {
-    pub(crate) complexity: u32,
-    pub(crate) message: String,
-}
-
-#[derive(Serialize, Deserialize, Debug)]
-pub struct MD5HashCash(pub(crate) MD5HashCashInput);
-
-#[derive(Serialize, Deserialize, Debug)]
-pub enum ChallengeType {
-    MD5HashCash(MD5HashCash),
-}
-
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Debug, Clone)]
 pub enum Message {
     Hello,
     Welcome { version: u8 },
@@ -105,7 +49,6 @@ impl MessageType {
     pub fn unicast(message: Message) -> MessageType {
         MessageType { message, message_type: ResponseType::Unicast }
     }
-    
 }
 
 
