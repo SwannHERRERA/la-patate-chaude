52d21591f9314db922600cb8fcd41fb27452482a
Noé Larrieu-Lacoste <noelarrieulacoste@yahoo.fr>
2022-07-01 12:33:51 +0200

    add main test challenge

--- recover_secret/src/main.rs
+++ recover_secret/src/main.rs
@@ -1,4 +1,6 @@
-use recover_secret::challenge_resolve::solve_secret_sentence_challenge;
+use recover_secret::challenge_resolve::{
+    solve_secret_sentence_challenge, solve_secret_string_challenge,
+};
 use recover_secret::file_utils::read_file;
 use recover_secret::models::RecoverSecretInput;
 use recover_secret::string_utils::generate_dictionary_hashmap;
@@ -17,16 +19,21 @@ fn main() {
         tuple_sizes: vec![5, 6, 5, 4, 2, 4, 5],
     };
 
+    // let recover_secret_input2: RecoverSecretInput = RecoverSecretInput {
+    //     word_count: 3,
+    //     letters: "iffiiilfatroridatol ft f".parse().unwrap(),
+    //     tuple_sizes: vec![3, 3, 3, 3, 3, 3, 3, 3],
+    // };
     let recover_secret_input2: RecoverSecretInput = RecoverSecretInput {
         word_count: 3,
-        letters: "iffiiilfatroridatol ft f".parse().unwrap(),
-        tuple_sizes: vec![3, 3, 3, 3, 3, 3, 3, 3],
+        letters: "iffiii".parse().unwrap(),
+        tuple_sizes: vec![3, 3],
     };
 
     println!("Solving challenge 1...");
-    // let output = solve_secret_sentence_challenge(&recover_secret_input1, &dictionary_hashmap);
-    // println!("{:?}", output);
+    let output = solve_secret_sentence_challenge(&recover_secret_input1, &dictionary_hashmap);
+    println!("{:?}", output);
     println!("Solving challenge 2...");
-    let output = solve_secret_sentence_challenge(&recover_secret_input2, &dictionary_hashmap);
+    let output = solve_secret_string_challenge(&recover_secret_input2);
     println!("{:?}", output);
 }
