9241ce12e42fec4cb7cc4f72946ff437be5b3003
Noé Larrieu-Lacoste <noelarrieulacoste@yahoo.fr>
2022-07-05 18:00:27 +0200

    update models

---
+++ monstrous_maze/src/models.rs
@@ -0,0 +1,27 @@
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub struct MonstrousMazeInput {
+    pub grid: String,
+    pub endurance: u8,
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub struct MonstrousMazeOutput {
+    pub path: String,
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub struct MonstrousMazeMap {
+    pub map: Vec<String>,
+    pub map_height: usize,
+    pub map_width: usize,
+    pub player_position: Position,
+    pub target_position: Position,
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone, Eq, PartialEq)]
+pub struct Position {
+    pub x: usize,
+    pub y: usize,
+}
