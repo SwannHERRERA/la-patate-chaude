2f03e35031327e17b9e14b854a6bdc66d83249c4
Noé Larrieu-Lacoste <noelarrieulacoste@yahoo.fr>
2022-07-07 16:39:29 +0200

    Add unit tests

--- monstrous_maze/src/challenge_resolve.rs
+++ monstrous_maze/src/challenge_resolve.rs
@@ -320,10 +320,16 @@ fn is_player_on_monster_position(map: &MonstrousMazeMap) -> bool {
 // test module
 #[cfg(test)]
 mod tests {
+    use std::collections::HashSet;
+
     use utils::file_utils::read_file;
 
-    use crate::challenge_resolve::MonstrousMazeResolver;
-    use crate::models::MonstrousMazeInput;
+    use crate::challenge_resolve::{
+        can_go_down, can_go_left, can_go_right, can_go_up, find_char_position_in_vec_map,
+        find_exit_position_from_vec_map, find_player_position_from_vec_map,
+        get_monstrous_maze_map_from_input, is_player_on_monster_position, MonstrousMazeResolver,
+    };
+    use crate::models::{MonstrousMazeInput, MonstrousMazeMap, Position};
 
     #[test]
     fn test_mazes_resolution() {
@@ -345,5 +351,230 @@ mod tests {
                 });
             assert_eq!(answer.path, answers[index]);
         })
+    }
+
+    #[test]
+    fn test_is_player_on_monster_position() {
+        let mut monstrous_maze_map = MonstrousMazeMap {
+            map: vec![
+                "# ###".to_string(),
+                "#   #".to_string(),
+                "# M #".to_string(),
+                "#   #".to_string(),
+                "#####".to_string(),
+            ],
+            map_height: 5,
+            map_width: 5,
+            player_position: Position { x: 1, y: 0 },
+            target_position: Position { x: 0, y: 0 },
+        };
+        assert_eq!(is_player_on_monster_position(&monstrous_maze_map), false);
+
+        monstrous_maze_map.player_position = Position { x: 2, y: 2 };
+        assert_eq!(is_player_on_monster_position(&monstrous_maze_map), true);
+    }
+
+    #[test]
+    fn test_can_go_left() {
+        let mut monstrous_maze_map = MonstrousMazeMap {
+            map: vec![
+                "# ###".to_string(),
+                "#   #".to_string(),
+                "# M #".to_string(),
+                "#   #".to_string(),
+                "#####".to_string(),
+            ],
+            map_height: 5,
+            map_width: 5,
+            player_position: Position { x: 1, y: 0 },
+            target_position: Position { x: 0, y: 0 },
+        };
+        let mut already_visited: HashSet<Position> = HashSet::new();
+        assert_eq!(can_go_left(&monstrous_maze_map, &already_visited), false);
+
+        monstrous_maze_map.player_position = Position { x: 2, y: 1 };
+        assert_eq!(can_go_left(&monstrous_maze_map, &already_visited), true);
+
+        already_visited.insert(Position { x: 1, y: 1 });
+        assert_eq!(can_go_left(&monstrous_maze_map, &already_visited), false);
+    }
+
+    #[test]
+    fn test_can_go_up() {
+        let mut monstrous_maze_map = MonstrousMazeMap {
+            map: vec![
+                "# ###".to_string(),
+                "#   #".to_string(),
+                "# M #".to_string(),
+                "#   #".to_string(),
+                "#####".to_string(),
+            ],
+            map_height: 5,
+            map_width: 5,
+            player_position: Position { x: 1, y: 0 },
+            target_position: Position { x: 0, y: 0 },
+        };
+        let mut already_visited: HashSet<Position> = HashSet::new();
+        assert_eq!(can_go_up(&monstrous_maze_map, &already_visited), false);
+
+        monstrous_maze_map.player_position = Position { x: 1, y: 2 };
+        assert_eq!(can_go_up(&monstrous_maze_map, &already_visited), true);
+
+        already_visited.insert(Position { x: 1, y: 1 });
+        assert_eq!(can_go_up(&monstrous_maze_map, &already_visited), false);
+    }
+
+    #[test]
+    fn test_can_go_right() {
+        let mut monstrous_maze_map = MonstrousMazeMap {
+            map: vec![
+                "# ###".to_string(),
+                "#   #".to_string(),
+                "# M #".to_string(),
+                "#   #".to_string(),
+                "#####".to_string(),
+            ],
+            map_height: 5,
+            map_width: 5,
+            player_position: Position { x: 1, y: 0 },
+            target_position: Position { x: 0, y: 0 },
+        };
+        let mut already_visited: HashSet<Position> = HashSet::new();
+        assert_eq!(can_go_right(&monstrous_maze_map, &already_visited), false);
+
+        monstrous_maze_map.player_position = Position { x: 2, y: 1 };
+        assert_eq!(can_go_right(&monstrous_maze_map, &already_visited), true);
+
+        already_visited.insert(Position { x: 3, y: 1 });
+        assert_eq!(can_go_right(&monstrous_maze_map, &already_visited), false);
+    }
+
+    #[test]
+    fn test_can_go_down() {
+        let mut monstrous_maze_map = MonstrousMazeMap {
+            map: vec![
+                "# ###".to_string(),
+                "#   #".to_string(),
+                "# M #".to_string(),
+                "#   #".to_string(),
+                "#####".to_string(),
+            ],
+            map_height: 5,
+            map_width: 5,
+            player_position: Position { x: 1, y: 3 },
+            target_position: Position { x: 0, y: 0 },
+        };
+        let mut already_visited: HashSet<Position> = HashSet::new();
+        assert_eq!(can_go_down(&monstrous_maze_map, &already_visited), false);
+
+        monstrous_maze_map.player_position = Position { x: 2, y: 1 };
+        assert_eq!(can_go_down(&monstrous_maze_map, &already_visited), true);
+
+        already_visited.insert(Position { x: 2, y: 2 });
+        assert_eq!(can_go_down(&monstrous_maze_map, &already_visited), false);
+    }
+
+    #[test]
+    fn test_find_char_position_in_vec_map() {
+        let map = vec![
+            "# ###".to_string(),
+            "#   #".to_string(),
+            "# M #".to_string(),
+            "#   #".to_string(),
+            "#####".to_string(),
+        ];
+
+        let result = find_char_position_in_vec_map(&'M', &map);
+        assert!(result.is_some());
+        assert_eq!(result.unwrap(), Position { x: 2, y: 2 });
+
+        let result = find_char_position_in_vec_map(&'I', &map);
+        assert!(result.is_none());
+    }
+
+    #[test]
+    fn test_find_exit_position_from_vec_map() {
+        let map = vec![
+            "# ###".to_string(),
+            "#   #".to_string(),
+            "# M #".to_string(),
+            "#   #".to_string(),
+            "###X#".to_string(),
+        ];
+
+        assert_eq!(
+            find_exit_position_from_vec_map(&map),
+            Position { x: 3, y: 4 }
+        );
+    }
+
+    #[test]
+    #[should_panic]
+    fn test_find_exit_position_from_vec_map_panic() {
+        let map = vec![
+            "# ###".to_string(),
+            "#   #".to_string(),
+            "# M #".to_string(),
+            "#   #".to_string(),
+            "#####".to_string(),
+        ];
+
+        find_exit_position_from_vec_map(&map);
+    }
+
+    #[test]
+    fn test_find_player_position_from_vec_map() {
+        let map = vec![
+            "#I###".to_string(),
+            "#   #".to_string(),
+            "# M #".to_string(),
+            "#   #".to_string(),
+            "#####".to_string(),
+        ];
+
+        assert_eq!(
+            find_player_position_from_vec_map(&map),
+            Position { x: 1, y: 0 }
+        );
+    }
+
+    #[test]
+    #[should_panic]
+    fn test_find_player_position_from_vec_map_panic() {
+        let map = vec![
+            "# ###".to_string(),
+            "#   #".to_string(),
+            "# M #".to_string(),
+            "#   #".to_string(),
+            "#####".to_string(),
+        ];
+
+        find_player_position_from_vec_map(&map);
+    }
+
+    #[test]
+    fn test_get_monstrous_maze_map_from_input() {
+        let monstrous_maze_input = MonstrousMazeInput {
+            endurance: 2,
+            grid: "#I###\n#   #\n# M #\n#   #\n###X#".to_string(),
+        };
+        let expected_monstrous_maze_map = MonstrousMazeMap {
+            map: vec![
+                "#I###".to_string(),
+                "#   #".to_string(),
+                "# M #".to_string(),
+                "#   #".to_string(),
+                "###X#".to_string(),
+            ],
+            map_height: 5,
+            map_width: 5,
+            player_position: Position { x: 1, y: 0 },
+            target_position: Position { x: 3, y: 4 },
+        };
+
+        assert_eq!(
+            get_monstrous_maze_map_from_input(&monstrous_maze_input),
+            expected_monstrous_maze_map
+        );
     }
 }
